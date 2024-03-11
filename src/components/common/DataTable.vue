5f6d87fa54b404d17f53f95c93a55adb7a524e3c
Carl Louie Aruta <arutacarllouie@gmail.com>
2024-03-11 14:50:33 +0800

    feat: add pagination handlers

--- src/components/common/DataTable.vue
+++ src/components/common/DataTable.vue
@@ -13,7 +13,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(r, key) in data" :key="key">
+        <tr v-for="(r, key) in dataSubset" :key="key">
           <td v-for="h in headers" :key="h.field" class="truncate-text">
             <slot :name="h.field" :data="r[h.field]">
               <span :title="r[h.field]">{{ r[h.field] }}</span>
@@ -26,20 +26,27 @@
     <footer class="table-paginator">
       <div class="items-per-page__container">
         <span>Items per page</span>
-        <select name="itemsPerPage" v-model="currentPerPage">
+        <select name="itemsPerPage" v-model="currentPerPage" @change="currentPage = 1">
           <option v-for="p in rowsPerPage" :key="p" :value="p">{{ p }}</option>
         </select>
       </div>
       <div class="paginator__container">
-        <font-awesome-icon :icon="faBackward" />
-        <font-awesome-icon :icon="faCaretLeft" />
+        <font-awesome-icon :icon="faBackward" class="cursor-pointer" @click="handlePagination('first')" />
+        <font-awesome-icon :icon="faCaretLeft" class="cursor-pointer" @click="handlePagination('prev')" />
         <div class="page__container">
           <span>Page</span>
-          <input v-model="currentPage" >
+          <input
+            ref="pageInputRef"
+            inputmode="numeric"
+            :value="currentPage"
+            @blur="onInputValueChange"
+            @keyup.enter="onInputValueChange"
+            @input="onInputValueChange"
+          >
           <span>{{ `of ${totalPages}` }}</span>
         </div>
-        <font-awesome-icon :icon="faForward" />
-        <font-awesome-icon :icon="faCaretRight" />
+        <font-awesome-icon :icon="faCaretRight" class="cursor-pointer" @click="handlePagination('next')" />
+        <font-awesome-icon :icon="faForward" class="cursor-pointer" @click="handlePagination('last')" />
       </div>
       <div class="total-items__container">
         {{ `Showing ${currentItemsCountMin > 0 ? currentItemsCountMin + ' - ' : ''}${currentItemsCount} of ${totalItems}` }}
@@ -70,21 +77,66 @@ const props = defineProps({
     type: Array as PropType<Array<number>>,
     default: () => ([10, 25, 50]),
   },
+  serverSidedMode: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const currentPage = ref<number>(1);
 const currentPerPage = ref<number>(props.rowsPerPage[0] || 10);
+const pageInputRef = ref<HTMLInputElement>();
 
-const totalPages = computed(() => (props.totalItems / currentPerPage.value) || 1)
+const dataSubset = computed(() => {
+  return props.data.slice(currentItemsCountMin.value - 1, currentItemsCount.value);
+});
+const totalPages = computed(() => Math.ceil(props.totalItems / currentPerPage.value) || 1)
 const currentItemsCount = computed(() => {
   const count = currentPage.value * currentPerPage.value;
 
   return count > props.totalItems ? props.totalItems : count;
 });
 const currentItemsCountMin = computed(() => {
-  const min = currentItemsCount.value - currentPerPage.value + 1;
-  return min > 0 ? min : 0;
+  return ((currentPage.value - 1) * currentPerPage.value) + 1;
 });
+
+const handlePagination = (action: 'first' | 'last' | 'prev' | 'next') => {
+  switch (action) {
+    case 'first': {
+      currentPage.value = 1;
+      break;
+    }
+    case 'last': {
+      currentPage.value = totalPages.value;
+      break;
+    }
+    case 'prev': {
+      !(currentPage.value <= 1) && currentPage.value--;
+      break;
+    }
+    case 'next': {
+      !(currentPage.value >= totalPages.value) && currentPage.value++;
+      break;
+    }
+  }
+};
+const onInputValueChange = ($event: any) => {
+  console.warn($event)
+  console.warn($event.target.value)
+
+  if ($event instanceof InputEvent) {
+    console.warn('input')
+    pageInputRef.value!.value = pageInputRef!.value!.value.replace(/\D+/g, '');
+    
+    if (+pageInputRef.value!.value <= 0) {
+      pageInputRef.value!.value = '1';
+    } else if (+pageInputRef.value!.value > totalPages.value) {
+      pageInputRef.value!.value = '' + totalPages.value;
+    }
+  } else {
+    currentPage.value = $event.target.value;
+  }
+}
 </script>
 
 <style scoped>
@@ -115,12 +167,18 @@ const currentItemsCountMin = computed(() => {
   .table-paginator {
     @apply flex justify-between px-4 text-sm h-16 items-center border-t border-t-[#C3CDC9] text-[#3C5B51];
 
+    .items-per-page__container,
+    .total-items__container,
+    .paginator__container {
+      @apply basis-1/3;
+    }
+
     select {
       @apply w-12 text-center border-[#A5B3AF] border-[1px] rounded-[4px] mx-2;
     }
 
     .paginator__container {
-      @apply flex gap-2 items-center;
+      @apply flex gap-2 items-center justify-center;
 
       input {
         @apply w-14 text-center border-[#A5B3AF] border-[1px] rounded-[4px] mx-2;
@@ -133,6 +191,10 @@ const currentItemsCountMin = computed(() => {
       .page__container > span {
         @apply text-sm;
       }
+    }
+
+    .total-items__container {
+      @apply text-right;
     }
   }
 }
